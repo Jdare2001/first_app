215569bf25c92ab841ca4f41cbcb56fc067b67b7
Jayden <59065556+Jdare2001@users.noreply.github.com>
2023-06-13 21:57:53 +0100

    Update gradent_container.dart
    added const variables to improve readability and improve the robustness

--- lib/gradent_container.dart
+++ lib/gradent_container.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:first_app/styled_text.dart';
 
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
@@ -13,8 +16,8 @@ class GradientContainer extends StatelessWidget {
             Color.fromARGB(255, 93, 59, 150),
             Color.fromARGB(255, 35, 11, 73),
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(
